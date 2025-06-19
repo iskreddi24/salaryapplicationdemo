f5db217b98eb82f1c4e8b0c16ca3d47800d237cd
iskreddi24 <sivakrishnareddyinduri.com>
2025-06-19 09:18:20 +0530

    second commit

--- src/main/java/com/application/employeesalary/RestControllerExample.java
+++ src/main/java/com/application/employeesalary/RestControllerExample.java
@@ -22,6 +22,46 @@ public class RestControllerExample {
 
         return "";
     }
+    @GetMapping(path="/employee/details")
+    public EmployeeDetails getEmployeeDetails(){
+        EmployeeDetails employeeDetails=new EmployeeDetails();
+        employeeDetails.setEmployeeName("ravi kiran samrat");
+        EmpDesignation empDesignation=new EmpDesignation();
+        empDesignation.setFulltimeOrParttime("FULLTIME");
+        EmpDesignationType empDesignationType=new EmpDesignationType();
+        empDesignationType.setJobtype("SOFTWARE");
+        empDesignation.setEmpDesignationType(empDesignationType);
+        employeeDetails.setEmpDesignation(empDesignation);
+        SalaryDetails salaryDetails=new SalaryDetails();
+        salaryDetails.setProfessionalTax(3000);
+        salaryDetails.setPfDeduction(1500);
+        salaryDetails.setBonusSalary(60000);
+        salaryDetails.setIncomeTax(2300);
+        salaryDetails.setSalary(66000);
+        employeeDetails.setSalaryDetails(salaryDetails);
+        LoanDetails loanDetails=new LoanDetails();
+        loanDetails.setHouseLoan(55000);
+        loanDetails.setCarLoan(30000);
+        employeeDetails.setLoanDetails(loanDetails);
+        InvestmentsDetails investmentsDetails=new InvestmentsDetails();
+        investmentsDetails.setBankInvestment(200000);
+        RealEstate realEstate=new RealEstate();
+        realEstate.setNoOfPlots(3);
+        investmentsDetails.setRealEstate(realEstate);
+        employeeDetails.setInvestmentsDetails(investmentsDetails);
+        InsuranceDetails insuranceDetails=new InsuranceDetails();
+        insuranceDetails.setCarinsurance(2000);
+        FamilyInsurance familyInsurance=new FamilyInsurance();
+        familyInsurance.setIciciLomabadInsurance(24000);
+        IndividualHealthInsurance individualHealthInsurance=new IndividualHealthInsurance();
+        individualHealthInsurance.setLicTermInsurance(20000);
+        insuranceDetails.setHouseInsurance(20000);
+        insuranceDetails.setIndividualHealthInsurance(individualHealthInsurance);
+        insuranceDetails.setFamilyInsurance(familyInsurance);
+        employeeDetails.setInsuranceDetails(insuranceDetails);
+        return employeeDetails;
+
+    }
     @PostMapping(path = "/calculate/net-salary")
     public double calculateNetSalary(@RequestBody EmployeeDetails employeeDetails) {
 
@@ -54,12 +94,18 @@ public class RestControllerExample {
 
         return loanDetails.getCarLoan() + loanDetails.getHouseLoan();
     }
+    @GetMapping(path = "/get/insurancedetails")
+    public InsuranceDetails getTotalInsurance(){
+        EmployeeDetails employeeDetails=new EmployeeDetails();
+        InsuranceDetails insuranceDetails=new InsuranceDetails();
+        employeeDetails.setInsuranceDetails(insuranceDetails);
+        insuranceDetails.setHouseInsurance(50000);
+        insuranceDetails.setCarinsurance(15000);
+        return insuranceDetails;
+    }
     @PostMapping(path = "/calculate/total-insurance")
     public double calculateTotalInsurance(@RequestBody EmployeeDetails employeeDetails) {
-
         InsuranceDetails insuranceDetails = employeeDetails.getInsuranceDetails();
-        insuranceDetails.setHouseInsurance(30000);
-        insuranceDetails.setCarinsurance(1500);
         if (insuranceDetails == null) {
             return 0.0;
         }
